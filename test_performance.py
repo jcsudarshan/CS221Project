a1d97e04c8aa777a643ef1613f11c0022afcabb1
Ben Peloquin <bpeloqui@stanford.edu>
2015-10-17 22:07:42 -0700

    beginning of testing suite

---
+++ test_performance.py
@@ -0,0 +1,39 @@
+##############################################################################
+##############################################################################
+# Testing suite for algorithms
+#import algorithms.baseline
+
+import baseline
+import util_baseline
+
+# Current data sets
+# Perhaps capture in second and third args
+# current dir is CS221Project/tests/
+train_path = "polarity.dev"
+dev_path = "polarity.train"
+
+# populate lis of algorithms we'd like to run
+current_tests = "baseline"
+
+# Baseline performance
+# ---------------------
+if "baseline" in current_tests:
+	current_alg = "baseline"
+	print("Testing " + current_alg + " on " + train_path + " and " + dev_path + "...")
+
+	# prelim set-up
+	train = util_baseline.readExamples(train_path)
+	dev = util_baseline.readExamples(dev_path)
+	featureExtractor = baseline.extractWordFeatures
+	
+	# calculate weights
+	weights = baseline.learnPredictor(train, dev, featureExtractor, eta=0.1, numIters=10)
+
+	# performance analysis
+	trainError = util_baseline.evaluatePredictor(train, lambda(x) : \
+		(1 if util_baseline.dotProduct(featureExtractor(x), weights) >= 0 else -1))
+	devError = util_baseline.evaluatePredictor(dev, lambda(x) : \
+    	(1 if util_baseline.dotProduct(featureExtractor(x), weights) >= 0 else -1))
+
+	print trainError
+	print devError
