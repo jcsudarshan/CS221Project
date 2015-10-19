b8a002fc5cb1a13ab6efde6676c848c6f994a70d
Ben Peloquin <bpeloqui@stanford.edu>
2015-10-18 17:22:52 -0700

    outputs unlabled and labeled data for given original polarity formatting

--- dev/baseline/annotate50.py
+++ dev/baseline/annotate50.py
@@ -3,15 +3,22 @@ import random
 
 file_path = "../../data/"
 file_name = "polarity.dev"
-aFile_name = "annotate_" + file_name
+lFile_name = "annotateL_" + file_name 	# labled data
+ulFile_name = "annotateUL_" + file_name 	# unlabled data
 
 
 random.seed(10)
 examples = readExamples(file_path + file_name)		# read in examples from data file
 sample = random.sample(examples, 50) 			# grab a random sample of 50 items
 
-out = open(file_path + aFile_name, 'w')
+out = open(file_path + lFile_name, 'w')
 for x, y in sample:
 	example = str(y) + ' ' + x + '\n'
+	out.write(example)
+out.close()
+
+out = open(file_path + ulFile_name, 'w')
+for x, y in sample:
+	example = x + '\n'
 	out.write(example)
 out.close()
